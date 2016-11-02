f6e4353b7da606314ced39b9045d300239295f63
Yang Deng <d2894861@gmail.com>
2016-11-02 01:55:43 -0400

    Finish basic customized Bcrypt algorithm

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bcrypt
+========
+
+[![Build Status](https://travis-ci.org/Polarising/bcrypt.png?branch=master)](http://travis-ci.org/Polarising/bcrypt)
+
 Instead of using PHP hash password API, encrypt plain text by using Bcrypt algorithm, and make sure it's compatible with other languages, like Java, python
---
+++ tests/BcryptTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Bcrypt\Test;
+
+use Bcrypt\Bcrypt;
+
+class BcryptTest extends \PHPUnit_Framework_TestCase
+{
+	public function testCanEncrypt()
+	{
+		$bcrypt = new Bcrypt();
+		$plaintext = '123456';
+		$ciphertext = $bcrypt->encrypt($plaintext);
+		$this->assertNotEquals('*0');
+	}
+
+	public function testCanVerify()
+	{
+		$bcrypt = new Bcrypt();
+		$plaintext = '123456';
+		$ciphertext = $bcrypt->encrypt($plaintext);
+		$this->assertTrue($bcrypt->verify($plaintext, $ciphertext));
+	}
+}
