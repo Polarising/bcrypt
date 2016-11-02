e8132cea91a7349e4c64e7152631f3f0e1fc65d8
Yang Deng <d2894861@gmail.com>
2016-11-02 02:03:41 -0400

    remove Jmes part in test/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,3 @@ error_reporting(-1);
 date_default_timezone_set('UTC');
 // Include the composer autoloader
 $loader = require __DIR__ . '/../vendor/autoload.php';
-// Clear out any previously compiled JMESPath files.
-JmesPath\Env::cleanCompileDir();
